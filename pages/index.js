3be1c70988c31db06881aac5463ca64af84a1ecf
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2026-01-20 00:33:24 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home()
+{
+    return <h1>Vou sentir falta dessa parceria tão boa que construímos. 
+Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa nova fase. git diffBoa sorte!</h1>
+} // Home
+
+export default Home;
