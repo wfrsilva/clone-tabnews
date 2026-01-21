9e3dbc26fb9f41131426fe5435cddaab8b01bc61
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2026-01-21 23:11:24 +0000

    Colocando foto codorna

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home()
 {
-    return <h1>Vou sentir falta dessa parceria tão boa que construímos.<br></br> 
-Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa nova fase.<br></br> Boa sorte!</h1>
+    return <><h1>Vou sentir falta dessa parceria tão boa que construímos.<br></br>
+        Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa nova fase.<br></br> Boa sorte!</h1>
+        <img src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26528/codornas-cpt.jpg"></img></>
 } // Home
 
 export default Home;
