ed03d8df2c7c15a44250c69b5101383968120dec
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2026-01-21 23:20:52 +0000

    Trocada URL da foto

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home()
 {
     return <><h1>Vou sentir falta dessa parceria tão boa que construímos.<br></br>
         Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa nova fase.<br></br> Boa sorte!</h1>
-        <img src="imgs/WhatsApp Image 2026-01-21 at 20.15.19.jpg"></img></>
+        <img src="pages/imgs/WhatsApp Image 2026-01-21 at 20.15.19.jpg"></img>
+        <img src="/workspaces/clone-tabnews/pages/imgs/WhatsApp Image 2026-01-21 at 20.15.19.jpg"></img></>
 } // Home
 
 export default Home;
