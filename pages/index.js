ac64ccbe352f3ea2411f8cc3dc94e46218296684
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2026-02-18 19:10:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-function Home()
-{
-    return <><h1>Vou sentir falta dessa parceria tão boa que construímos.<br></br>
-        Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa nova fase.<br></br> Boa sorte!</h1>
-        <img src="imgs/foto.jpg"></img>
-        </>
+function Home() {
+  return (
+    <>
+      <h1>
+        Vou sentir falta dessa parceria tão boa que construímos.<br></br>
+        Mas fico muito orgulhoso de ti e torcendo muito pelo teu sucesso nessa
+        nova fase.<br></br> Boa sorte!
+      </h1>
+      <img src="imgs/foto.jpg"></img>
+    </>
+  );
 } // Home
 
 export default Home;
